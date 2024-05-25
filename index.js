f7c13ff71d3b80653417999facd47878604aff69
jingkenneth <jingkenneth123@gmail.com>
2024-05-25 19:14:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
 }
-function clickDisLike1() {
-  let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
-  countDisLikes1.textContent = totalDisLikes1.toString()  
-}
 function clickLike2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
   countLikes2.textContent = totalLikes2.toString()
+}
+function clickDisLike1() {
+  let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
+  countDisLikes1.textContent = totalDisLikes1.toString()  
 }
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
